bda4a92185c350d889e1e87d5e8af0c8ed9d072e
Mahi-272006 <mahimakumari24323@gmail.com>
2025-07-15 04:06:17 +0530

    Update App.py

--- App.py
+++ App.py
@@ -110,6 +110,8 @@ def clear_session():
     return resp
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
 
 
