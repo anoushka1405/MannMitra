ba573a09092e5ebeaf9bf45261ae51b9d23f7fef
Anoushka Gupta <anoushka142006@gmail.com>
2025-07-15 17:33:29 +0530

    Update App.py

--- App.py
+++ App.py
@@ -110,7 +110,9 @@ def clear_session():
     return resp
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
